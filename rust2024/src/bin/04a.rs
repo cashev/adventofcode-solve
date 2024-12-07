c5025ea26ec072dacc5af716ef9c809c3f975fac
cashev <42924021+cashev@users.noreply.github.com>
2024-12-07 13:34:05 +0900

    fmt

--- rust2024/src/bin/04a.rs
+++ rust2024/src/bin/04a.rs
@@ -12,18 +12,18 @@ fn search_xmas(lines: &str) -> i32 {
 
     // 横方向のチェック
     for y in 0..height {
-        for x in 0..width-3 {
-            let s: String = chars[y][x..x+4].iter().collect();
+        for x in 0..width - 3 {
+            let s: String = chars[y][x..x + 4].iter().collect();
             if s == "XMAS" || s == "SAMX" {
                 count += 1;
             }
         }
     }
 
-    // 縦方向のチェック 
+    // 縦方向のチェック
     for x in 0..width {
-        for y in 0..height-3 {
-            let s: String = (0..4).map(|i| chars[y+i][x]).collect();
+        for y in 0..height - 3 {
+            let s: String = (0..4).map(|i| chars[y + i][x]).collect();
             if s == "XMAS" || s == "SAMX" {
                 count += 1;
             }
@@ -31,9 +31,9 @@ fn search_xmas(lines: &str) -> i32 {
     }
 
     // 斜め方向のチェック(右下)
-    for y in 0..height-3 {
-        for x in 0..width-3 {
-            let s: String = (0..4).map(|i| chars[y+i][x+i]).collect();
+    for y in 0..height - 3 {
+        for x in 0..width - 3 {
+            let s: String = (0..4).map(|i| chars[y + i][x + i]).collect();
             if s == "XMAS" || s == "SAMX" {
                 count += 1;
             }
@@ -41,9 +41,9 @@ fn search_xmas(lines: &str) -> i32 {
     }
 
     // 斜め方向のチェック(左下)
-    for y in 0..height-3 {
+    for y in 0..height - 3 {
         for x in 3..width {
-            let s: String = (0..4).map(|i| chars[y+i][x-i]).collect();
+            let s: String = (0..4).map(|i| chars[y + i][x - i]).collect();
             if s == "XMAS" || s == "SAMX" {
                 count += 1;
             }
